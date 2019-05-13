51b3f49ca75704400055dddc0b9d4879ebefce09
Lyon Belyansky <lyon.belyansky@dawsoncollege.qc.ca>
2019-05-13 13:10:39 -0400

    test for report file

--- ErrorAnalysis.md
+++ ErrorAnalysis.md
@@ -1,7 +1,15 @@
-# Title
-## Course - Section
-## Authors
+# Report: Error Analysis
+## 360-420-w2019 - Section 
+## Jeremy Lackmann-Mincoff and Lyon Belyansky
 
 ## Distributions of Model Accuracy
+Each time we run the model, we are getting a different accuracy because the model is being trained on a randomly shuffled training dataset. 
+
+Running our model 1000 times, we get a mean accuracy of 96.9% and a standard deviation of 1.28. This shows that our model is getting a relatively good accuracy when tested on unseen data.
+
+A sensible baseline could be to predict that all patients have a malignant tumor. In such a case, we would get an accuracy of 34.8%. On the other hand, if we predict all benign tumors, we would get an accuracy of 65.2%. 
 
 ## Analysis of different error types
+A False Positive is a prediction of a malignant tumor whereas it is actually benign.
+A False Negative is a prediction of a bening tumor whereas it is acutally malignant. 
+It is clear that to make a False Negative is a much worse mistake in a medical context than to make a False Positive.
--- kNNMain.java
+++ kNNMain.java
@@ -12,14 +12,14 @@ public class kNNMain{
     
     String PATH_TO_DATA = args[0];
     
-    List<DataPoint> myList = DataSet.readDataSet(PATH_TO_DATA);
-    
-    DataPoint dp1 = myList.get(0);
-    DataPoint dp2 = myList.get(1);
-    
-    double[] dpx = dp1.getX();
-    
-    String printableX = Arrays.toString(dpx);
+//    List<DataPoint> myList = DataSet.readDataSet(PATH_TO_DATA);
+//    
+//    DataPoint dp1 = myList.get(0);
+//    DataPoint dp2 = myList.get(1);
+//    
+//    double[] dpx = dp1.getX();
+//    
+//    String printableX = Arrays.toString(dpx);
     
 //    System.out.println(printableX);
 //    System.out.println(dp1.getLabel());
@@ -27,8 +27,8 @@ public class kNNMain{
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
     
-    List<DataPoint> test = DataSet.getTestSet(myList, 0.2);
-    List<DataPoint> train = DataSet.getTrainingSet(myList, 0.8);
+//    List<DataPoint> test = DataSet.getTestSet(myList, 0.2);
+//    List<DataPoint> train = DataSet.getTrainingSet(myList, 0.8);
     
 //    DataSet.printDataSet(test);
 //    System.out.println();
@@ -41,39 +41,72 @@ public class kNNMain{
 //    System.out.println("train:");
 //    DataSet.printLabelFrequencies(train);
 
-    // TASK 3: Use the DataSet class methods to plot the 2D data (binary and multi-class)
-
-
-
     // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
     // and returns the Euclidean distance between those two points (as a double)
     
-    double dist = DataSet.distanceEuclid(dp1, dp2);
+//    double dist = DataSet.distanceEuclid(dp1, dp2);
 //    System.out.println(dist);
 
 
     // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
     // and make a print a predicted target label
     
-    KNNClassifier object = new KNNClassifier(5);
-    DataPoint[] kNearest = object.getNearestNeighbors(myList, dp1);
-    String myLabel = object.predict(myList, kNearest[0]);
+//    KNNClassifier object = new KNNClassifier(5);
+//    DataPoint[] kNearest = object.getNearestNeighbors(myList, dp1);
+//    String myLabel = object.predict(myList, kNearest[0]);
 //    System.out.println(myLabel);
 
     // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
-    System.out.println(kNearest.length);
-    int count = 0;
-    for(int i = 0; i < test.size(); i++){
-      DataPoint wht = test.get(i);
-      String predicted = object.predict(train, wht);
-      if (predicted.equals(wht.getLabel())){
-        count ++; 
-      }
-    }
-    System.out.println(count); 
-    System.out.println((double) count/test.size()*100);
+    //System.out.println(kNearest.length);
+//    int count = 0;
+//    for(int i = 0; i < test.size(); i++){
+//      DataPoint wht = test.get(i);
+//      String predicted = object.predict(train, wht);
+//      if (predicted.equals(wht.getLabel())){
+//        count ++; 
+//      }
+//    }
+    //System.out.println(count); 
+    //System.out.println((double) count/test.size()*100);
     
+    /////////////////
+    //1000 times/////
+    /////////////////
+    KNNClassifier object = new KNNClassifier(5);
+//    String predicted;
+    int counter = 0;
+    double[] sum = new double[1000];
+    for (int i = 0; i < 1000; i++)
+    {
+    List<DataPoint> myList = DataSet.readDataSet(PATH_TO_DATA);
+    List<DataPoint> test = DataSet.getTestSet(myList, 0.3);
+    List<DataPoint> train = DataSet.getTrainingSet(myList, 0.7);
+
+//      test = DataSet.getTestSet(myList, 0.3);
+//      train = DataSet.getTrainingSet(myList, 0.7);
+      
+      for(int e = 0; e < test.size(); e++)
+      {
+        DataPoint myDP = test.get(e);
+        String predicted = object.predict(train, myDP);
+        if (predicted.equals(myDP.getLabel()))
+        {
+          counter ++; 
+        }
+      }
+      double dCounter = (double) (counter);
+      double dSize = (double) (test.size());
+      double accuracy = (dCounter/dSize) *100;
+      sum[i] = accuracy;
+      //System.out.println(sum[i]);
+      counter = 0;
+    }
+    double myMean = mean(sum);
+    double mySD = standardDeviation(sum);
+    System.out.println("Mean: " + myMean + "\tSD " + mySD);
+    List<DataPoint> tList = DataSet.readDataSet(PATH_TO_DATA);
+    DataSet.printLabelFrequencies(tList);
   }
 
   public static double mean(double[] arr){
